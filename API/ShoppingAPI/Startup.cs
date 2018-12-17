cf1aeae6a8b37be1765c04dba3ea1281c2d3b913
dmdmaasz6 <dmdmaasz6@users.noreply.github.com>
2018-12-17 22:00:55 +0200

    Opperational Shopping Cart Feature in Angular

--- API/ShoppingAPI/Startup.cs
+++ API/ShoppingAPI/Startup.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +29,13 @@ namespace ShoppingAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<IdentityDbContext>(options => options.UseSqlite("Data Source=users.sqlite",
+                optionsBuilder => optionsBuilder.MigrationsAssembly("ShoppingAPI")));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +49,11 @@ namespace ShoppingAPI
             {
                 app.UseHsts();
             }
+
+            app.UseIdentity();
+            app.UseMvcWithDefaultRoute();
+
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod());
 
             app.UseHttpsRedirection();
             app.UseMvc();
--- Client/Shopping/src/app/app.component.ts
+++ Client/Shopping/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Shopping';
+  results = [];
+  cart = [];
+  addToCart = function (item){
+    this.cart.push(item);
+    alert(item.name + ' was added to your cart for N$' + item.price);
+  }
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit() : void {
+    interface Product{
+      name: string;
+      category: string;
+      price: number;
+    }
+    this.http.get<Product>('https://localhost:44328/api/products').subscribe(data => {
+      // this.results = data;
+      for (let i in data) this.results.push(data[i]);
+    });
+  }
 }
