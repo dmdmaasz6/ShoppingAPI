499003895e531ad2712d1b8710be76ba981334b7
Donovan Maasz <dmdmaasz6.reg@outlook.com>
2018-12-17 15:41:36 +0200

    Completed the basic api for retrieving Products

--- ShoppingAPI/Controllers/ProductsController.cs
+++ ShoppingAPI/Controllers/ProductsController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ShoppingAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ShoppingAPI.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Products")]
+    [Route("api/Products/")]
     public class ProductsController : Controller
     {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Mechnical Keyboard", Category = "Electronics", Price = 900},
+            new Product { Id = 2, Name = "Aesop's Fables", Category = "Books", Price = 230.50M},
+            new Product { Id = 3, Name = "Tooth Paste", Category = "Toiletries", Price = 30},
+            new Product { Id = 4, Name = "Couch", Category = "Furniture", Price = 1600}
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public IActionResult GetById(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
 
     }
 }
--- ShoppingAPI/Models/Product.cs
+++ ShoppingAPI/Models/Product.cs
@@ -11,6 +11,6 @@ namespace ShoppingAPI.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
     }
 }
